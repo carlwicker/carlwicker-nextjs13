71591f6e0084e4db06441641df5876e94f74a740
Carl Wicker <carlwicker@gmail.com>
2022-12-03 13:59:40 +0000

    footer fix

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -4,7 +4,7 @@ import TailwindCssLogo from "./TailwindCssLogo";
 export default function Footer() {
   return (
     <div className="container flex gap-2  text-white text-xs mx-auto my-0 px-5 items-center">
-      <div>Made with</div>
+      <div className="whitespace-nowrap">Made with</div>
       <NextJsLogo />
       <div> & </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Footer from "./Footer";
-
 export default function Homepage() {
   return (
-    <div className="container mx-auto flex flex-col h-72 justify-center">
-      <h1 className="  px-5 text-4xl lg:text-8xl titlecase font-semibold text-white">
+    <div className="container mx-auto flex flex-col justify-center">
+      <h1 className="px-5 text-4xl lg:text-8xl titlecase font-bold text-gray-300">
         Front End Web Developer
       </h1>
-      <h2 className=" px-5 lg:p-5 lg:text-2xl text-base text-white">
-        HTML, CSS, JS, React, NextJS & TailwindCSS.
+      <h2 className=" px-5 lg:p-5 lg:text-2xl text-base font-thin text-gray-300">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
+        voluptates ducimus minima perferendis similique. Illum sit praesentium
+        temporibus rerum odit tempore aspernatur TailwindCSS.
       </h2>
     </div>
   );
